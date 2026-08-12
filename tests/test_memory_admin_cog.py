383b5f048408cdb05d916a1418983c813064325c
gumdropsmo22 <gumdropsmo22@gmail.com>
2026-08-12 14:49:18 +0300

    Test archived member ID validation

--- tests/test_memory_admin_cog.py
+++ tests/test_memory_admin_cog.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import unittest
 from types import SimpleNamespace
 
-from cogs.memory_admin import MemoryAdmin
+from cogs.memory_admin import MAX_DISCORD_USER_ID, MemoryAdmin, _parse_user_id
 
 
 class DummyResponse:
@@ -62,6 +62,28 @@ class MemoryAdminAuthorizationTests(unittest.IsolatedAsyncioTestCase):
 
         self.assertEqual(guild_id, 100)
         self.assertEqual(interaction.response.messages, [])
+
+
+class ArchivedMemberIdTests(unittest.TestCase):
+    def test_accepts_positive_discord_snowflake_string(self):
+        self.assertEqual(_parse_user_id("123456789012345678"), 123456789012345678)
+
+    def test_accepts_whitespace_around_valid_id(self):
+        self.assertEqual(_parse_user_id("  42  "), 42)
+
+    def test_rejects_non_decimal_ids(self):
+        for value in ("", "-1", "+1", "1.5", "abc", "<@123>"):
+            with self.subTest(value=value), self.assertRaises(ValueError):
+                _parse_user_id(value)
+
+    def test_rejects_zero_and_ids_above_64_bit_range(self):
+        with self.assertRaises(ValueError):
+            _parse_user_id("0")
+        with self.assertRaises(ValueError):
+            _parse_user_id(str(MAX_DISCORD_USER_ID + 1))
+
+    def test_accepts_maximum_64_bit_value(self):
+        self.assertEqual(_parse_user_id(str(MAX_DISCORD_USER_ID)), MAX_DISCORD_USER_ID)
 
 
 if __name__ == "__main__":
